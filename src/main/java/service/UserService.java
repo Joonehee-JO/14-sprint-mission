ae1e517f81a56f1d7dd704d3bbc55bbc248d7ca8
joonhee <parkes1801@naver.com>
2026-07-16 15:43:29 +0900

    feat: 유저서비스 구현

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,8 @@
+package service;
+
+import global.annotation.Comment;
+
+public interface UserService {
+    @Comment("최초 유저 생성 - 이름 입력 후 아이디발급")
+    Long initUser(String name);
+}
---
+++ src/main/java/service/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package service;
+
+import domain.User;
+import java.util.Optional;
+import repository.CrudRepository;
+
+public class UserServiceImpl implements UserService{
+    private final CrudRepository userRepository;
+
+    /*
+        롬복 못써서 생성자 열구 컨피그에서 주입하려합니다
+     */
+    public UserServiceImpl(CrudRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Long initUser(String name) {
+        Optional<User>user =userRepository.create(User.makeUser(name));
+        if(user.isEmpty()) throw new RuntimeException("에러 발생유");
+
+        return user.get().getUserId();
+    }
+}
