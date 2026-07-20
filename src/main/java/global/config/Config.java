c95448f74983100cee9f90e0c63a5947e52b2d8b
joonhee <parkes1801@naver.com>
2026-07-20 13:45:52 +0900

    feat: 의존성 주입 컨피그 클래스 작성

---
+++ src/main/java/global/config/Config.java
@@ -0,0 +1,58 @@
+package global.config;
+
+import controller.channel.ChannelController;
+import controller.message.MessageController;
+import controller.user.UserController;
+import repository.CrudRepository;
+import repository.UserRepository.UserRepositoryImpl;
+import repository.message.MessageRepository;
+import repository.message.MessageRepositoryImpl;
+import service.channel.ChannelService;
+import service.channel.ChannelServiceImpl;
+import service.message.MessageService;
+import service.message.MessageServiceImpl;
+import service.user.UserService;
+import service.user.UserServiceImpl;
+
+public class Config {
+    private final CrudRepository userRepository = new UserRepositoryImpl();
+    private final MessageRepository messageRepository = new MessageRepositoryImpl();
+    private final ChannelService channelService = new ChannelServiceImpl();
+    private final MessageService messageService = new MessageServiceImpl(messageRepository);
+    private final UserService userService = new UserServiceImpl(userRepository);
+    private final ChannelController channelController = new ChannelController(channelService);
+    private final MessageController messageController = new MessageController(messageService);
+    private final UserController userController = new UserController(userService);
+
+    public CrudRepository getUserRepository() {
+        return userRepository;
+    }
+
+    public MessageRepository getMessageRepository() {
+        return messageRepository;
+    }
+
+    public ChannelService getChannelService() {
+        return channelService;
+    }
+
+    public MessageService getMessageService() {
+        return messageService;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public ChannelController getChannelController() {
+        return channelController;
+    }
+
+    public MessageController getMessageController() {
+        return messageController;
+    }
+
+    public UserController getUserController() {
+        return userController;
+    }
+}
