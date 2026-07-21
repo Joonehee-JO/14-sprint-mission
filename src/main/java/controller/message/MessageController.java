f97b9d7667a33ccdf2adf25591a2ef93f63a8bbc
joonhee <parkes1801@naver.com>
2026-07-21 11:55:05 +0900

    fix: 메서드 리턴타입 수정

--- src/main/java/controller/message/MessageController.java
+++ src/main/java/controller/message/MessageController.java
@@ -12,12 +12,10 @@ public class MessageController {
         this.messageService = messageService;
     }
 
-    @Deprecated(since = "2026-07", forRemoval = false)  // 뷰 렌더링 부분 통합 후 구현하고자 미완성인거 알리려고 함
     @Comment("채널 컨트롤러에서 최초 채널 입장 / 메시지 입력마다 호출되는 컨트롤러")
-    public void channelAdmitExtractMessage(Long channelId){
-        List<Message> messageList = messageService.extractAllChannelMessage(channelId);
-
+    public List<Message> channelAdmitExtractMessage(Long channelId){
         //뷰 렌더링
+        return messageService.extractAllChannelMessage(channelId);
     }
 
     @Comment("채널 입장해서 메시지 입력 시 호출되는 컨트롤러")
