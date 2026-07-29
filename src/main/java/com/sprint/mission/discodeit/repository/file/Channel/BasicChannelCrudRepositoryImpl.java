e5bddb79e56e4d53921bc3f55890e1f0fb5bd138
joonhee <parkes1801@naver.com>
2026-07-29 18:47:17 +0900

    fix: 리포지토리 수정메서드  널 반환  수정

--- src/main/java/com/sprint/mission/discodeit/repository/file/Channel/BasicChannelCrudRepositoryImpl.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/Channel/BasicChannelCrudRepositoryImpl.java
@@ -23,12 +23,19 @@ public class BasicChannelCrudRepositoryImpl extends AbstractCrudRepository<Chann
 
     @Override
     public Channel updateEntity(UUID uuid, String updateName) {
+        /*
+            피드백 -> 이 부분 널 던지는거 이상하다
+         */
+        //서비스 계층에서 해당 엔터티를 찾아 넘겨준걸 업데이트하는게 맞다고봄
         Channel updateChannel = findById(uuid);
+
+        //서비스계층에서 확인된 개체를 던지므로 원래 널안씀
         if(updateChannel != null){
             updateChannel.update(updateName);
+            return updateChannel;
         }
 
-        return null;
+        throw new RuntimeException("디비 오류");
     }
 
     @Override
