cc0058e8bb5751eb537199dd13756031bbd223e8
joonhee <parkes1801@naver.com>
2026-07-30 15:40:59 +0900

    feat: 요구사항에 따른 새로운 도메인클래스 추가

---
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -0,0 +1,59 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/*
+    ----------todo 질문 리스트 -----------------
+    초기에 프로필 이미지인지 뭔지 그냥 채팅에만 파일을 첨부가능하게 하여
+    필드에 유저아이디와 채널아이디, 파일이 저장될 주소정도만 갖게 설계를 생각했는데
+    요구사항과 AI 를 통해 분석한 결과 요구사항에서는 어디서든 쓰일 수 있는 첨부파일 클래스를 만들고자 하였고
+    여기까진 OK 였는데 <- 이러면 하나의 클래스로 모든 파일 개체를 담당할 수 있으니까
+
+    필드를 설계해야하는데 그냥 여기 필드에 이게 프로필이미지 필드인지 채팅 필드에 작성된건지를 정의해서 만들고자하였지만(추가될때마다 새로운 필드를 추가하려했음 이넘으로라든지)
+    이건 그냥 주먹구구식 중복과 정규화를 생각안한 방법이구
+
+    --------AI 대안
+    // User
+    public class User {
+        UUID id;
+        UUID profileId; // BinaryContent의 PK를 FK로 참조
+    }
+
+    // Message
+    public class Message {
+        UUID id;
+        List<UUID> attachmentIds; // BinaryContent의 PK들을 FK로 참조
+    }
+
+    ⭕ DB 외래키(FK)를 완벽하게 걸 수 있음
+    User 테이블의 profile_id 컬럼은 BinaryContent 테이블의 id만 바라보는 명확한 FK가 됩니다. DB가 데이터 무결성을 확실하게 보장해 줍니다.
+
+    ⭕ 객체지향적 역할 분담 (파일은 파일 일만 함)
+    BinaryContent 입장에서는 "내가 어디에 쓰이는지"를 알 필요가 없습니다. 단순한 '파일 저장소의 파일 메타데이터 객체' 역할에 충실합니다.
+
+    파일이 어디에 쓰이는지는 파일을 가져다 쓰는 주체(User, Message)가 책임집니다.
+
+
+    라는데 솔직히 필드에 리스트로 뭔가를 들고있는단게 안와닿아서 이걸 AI 없이 설계할 수 있을까란 의문과 이거에 당연하게 익숙해져
+    설계하는걸 연습해야하는게 맞겠죠?
+
+    디비 테이블 그리는걸 연습해야할까요
+ */
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class BinaryContent {
+    UUID id;
+    String pathUrl;
+    String fileName; //필요할까? 나는 그냥 파일주소만 정의했었음.
+    String fileType; //이넘으로 정의해야할까?
+    @Builder.Default
+    Instant createdAd = Instant.now();
+}
---
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -0,0 +1,27 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+
+/*
+    어떤 유저가 어디 채널을 언제 마지막으로 읽었는지 체크하고
+    이로 그 이후의 메시지는 읽지않음으로 표시하기 위한 엔터티
+ */
+@Builder
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReadStatus {
+    UUID id;
+    UUID userId;
+    UUID channelId;
+    Instant latestReadAt;
+}
