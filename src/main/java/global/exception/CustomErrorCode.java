778460ac25ac8c2260b8aae8bf75b37246b9d239
joonhee <parkes1801@naver.com>
2026-07-31 18:13:04 +0900

    feat: 추가 예외 메시지 작성

--- src/main/java/global/exception/CustomErrorCode.java
+++ src/main/java/global/exception/CustomErrorCode.java
@@ -5,7 +5,10 @@ public enum CustomErrorCode {
     INVALID_USER_INIT("계정을 생성하고 진행해주세요"),
     INVALID_USER_NAME_EMPTY("이름을 제대로 입력해주세요"),
     INVALID_MESSAGE_EMPTY("문자 메시지를 입력하여 보내주세요"),
-    INVALID_MESSAGE_MAX_LENGTH("문자 메시지는 최대 500글자만 가능합니다");
+    INVALID_MESSAGE_MAX_LENGTH("문자 메시지는 최대 500글자만 가능합니다"),
+
+    //새로 추가부분(어떤 서비스 예외인지 다 나눠야 할듯)
+    INVALID_USER_DUPLICATE_EMAIL("이미 존재하는 계정입니다.");
 
     private final String message;
 
