fb8b6adb19641281b37a2ca981b167b61f0aa2c0
joonhee <parkes1801@naver.com>
2026-08-04 16:15:57 +0900

    refactor: 도메인 수정

--- src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -14,14 +14,21 @@ import lombok.experimental.FieldDefaults;
     어떤 유저가 어디 채널을 언제 마지막으로 읽었는지 체크하고
     이로 그 이후의 메시지는 읽지않음으로 표시하기 위한 엔터티
  */
-@Builder
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ReadStatus {
+public class ReadStatus implements IdMapper{
     UUID id = UUID.randomUUID();
     UUID userId;
     UUID channelId;
     Instant latestReadAt;
+
+    @Builder
+    public ReadStatus(UUID userId, UUID channelId) {
+        this.id = UUID.randomUUID();
+        this.userId = userId;
+        this.channelId = channelId;
+        this.latestReadAt = null;           //채널 입장이라는 조건이 발생할 때 값이 들어오게 하기 위함
+    }
 }
