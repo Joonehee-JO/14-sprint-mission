af764729f641a93ff6e4f0cc936e290df49827cf
joonhee <parkes1801@naver.com>
2026-08-05 17:42:11 +0900

    feat: 서비스 앱 작성

---
+++ src/main/java/com/sprint/mission/discodeit/service/MessageServiceApp.java
@@ -0,0 +1,85 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.service.binarycontent.BinaryContentService;
+import com.sprint.mission.discodeit.service.channel.ChannelService;
+import com.sprint.mission.discodeit.service.message.MessageService;
+import com.sprint.mission.discodeit.web.controller.dto.req.CreateMessageRequestDTO;
+import com.sprint.mission.discodeit.web.controller.dto.req.MessageUpdateRequestDTO;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MessageServiceApp {
+    private final MessageService messageService;
+    private final ChannelService channelService;
+    private final BinaryContentService binaryContentService;
+
+/*
+    MessageService 고도화
+
+    create
+    [ ] 선택적으로 여러 개의 첨부파일을 같이 등록할 수 있습니다.
+    [ ] DTO를 활용해 파라미터를 그룹화합니다.
+
+    findAll
+    [ ] 특정 Channel의 Message 목록을 조회하도록 조회 조건을 추가하고, 메소드 명을 변경합니다. findallByChannelId
+
+    update
+    [ ] DTO를 활용해 파라미터를 그룹화합니다.
+    수정 대상 객체의 id 파라미터, 수정할 값 파라미터
+
+    delete
+    [ ] 관련된 도메인도 같이 삭제합니다.
+    첨부파일(BinaryContent)
+ */
+
+    public Message createMessage(CreateMessageRequestDTO createMessageRequestDTO){
+        Message message = Message.init(createMessageRequestDTO.getUserId(),createMessageRequestDTO.getChannelId(), createMessageRequestDTO.getContent());
+
+        /*
+            1. dto 에 파일이 들어있는지 확인
+            2. 있으면 바이너리컨텐트서비스 호출
+            3. 메시지 서비스 호출
+         */
+
+        List<BinaryContent> storedBinaryContents = null;
+        List<UUID> filteredBinaryContents = null;
+        if(Objects.nonNull(createMessageRequestDTO.getImageList())){
+            storedBinaryContents = binaryContentService.storeFiles(createMessageRequestDTO.getImageList());
+            //서비스 필요형태로 변환
+            filteredBinaryContents = storedBinaryContents.stream()
+                .map(BinaryContent::getId)
+                .toList();
+        }
+
+        return messageService.createMessage(message, filteredBinaryContents);
+    }
+
+
+    public List<Message> findAllMessageByChannelId(UUID channelId){
+        return messageService.findAllMessageByChannelId(channelId);
+    }
+
+//    update
+//    [ ] DTO를 활용해 파라미터를 그룹화합니다.
+//    수정 대상 객체의 id 파라미터, 수정할 값 파라미터
+//
+//        delete
+//    [ ] 관련된 도메인도 같이 삭제합니다.
+//    첨부파일(BinaryContent)
+
+    public Message updateMessage(MessageUpdateRequestDTO messageUpdateRequestDTO){
+        return messageService.updateMessageContent(messageUpdateRequestDTO.getMessageId(), messageUpdateRequestDTO.getContent());
+    }
+
+    public void deleteMessage(UUID messageId){
+        messageService.deleteMessage(messageId);
+        //todo binaryContentService.
+    }
+}
---
+++ src/main/java/com/sprint/mission/discodeit/service/ReadStatusServiceApp.java
@@ -0,0 +1,50 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.service.channel.ChannelService;
+import com.sprint.mission.discodeit.service.readstatus.ReadStatusService;
+import com.sprint.mission.discodeit.service.user.UserService;
+import com.sprint.mission.discodeit.web.controller.dto.req.ReadStatusCreateRequestDTO;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/*
+    todo ----------- 복습용
+    다른 서비스에서 PK 로 쓰이는게 들어오면 S1 에서 간접 검증을 진행한다
+ */
+@RequiredArgsConstructor
+@Service
+public class ReadStatusServiceApp {
+    private final ReadStatusService readStatusService;
+    private final UserService userService;
+    private final ChannelService channelService;
+
+    public ReadStatus createReadStatus(ReadStatusCreateRequestDTO readStatusCreateRequestDTO){
+        //간접 검증부
+        userService.findById(readStatusCreateRequestDTO.getUserId());
+        channelService.findChannelById(readStatusCreateRequestDTO.getChannelId());
+        ReadStatus readStatus = ReadStatus.init(readStatusCreateRequestDTO.getUserId(), readStatusCreateRequestDTO.getChannelId());
+
+        return readStatusService.createReadStatus(readStatus);
+    }
+
+    public ReadStatus findReadStatusById(UUID readStatusId){
+        return readStatusService.findReadStatusById(readStatusId);
+    }
+
+    public List<ReadStatus> findAllReadStatusByUserId(UUID userId){
+        userService.findById(userId);
+
+        return readStatusService.findReadStatusByUserId(userId);
+    }
+
+    public ReadStatus updateReadStatus(UUID readStatusId){
+        return readStatusService.updateReadStatusReadTime(readStatusId);
+    }
+
+    public void deleteReadStatus(UUID readStatusId){
+        readStatusService.deleteReadStatusById(readStatusId);
+    }
+}
