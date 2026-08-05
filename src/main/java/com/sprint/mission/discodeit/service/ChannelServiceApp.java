a8045fd5f02607c614d3a9f9d25acb09aa4f664c
joonhee <parkes1801@naver.com>
2026-08-05 17:47:26 +0900

    refactor: 채널 서비스 앱 리팩토링

--- src/main/java/com/sprint/mission/discodeit/service/ChannelServiceApp.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelServiceApp.java
@@ -4,18 +4,17 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.ReadStatus;
-import com.sprint.mission.discodeit.entity.ReadStatus.ReadStatusBuilder;
-import com.sprint.mission.discodeit.entity.User;
+
 import com.sprint.mission.discodeit.service.channel.ChannelService;
 import com.sprint.mission.discodeit.service.message.MessageService;
 import com.sprint.mission.discodeit.service.readstatus.ReadStatusService;
-import com.sprint.mission.discodeit.service.user.UserService;
-import com.sprint.mission.discodeit.web.controller.channel.dto.ChannelCreateRequestDTO;
-import com.sprint.mission.discodeit.web.controller.channel.dto.PrivateChannelCreateRequestDTO;
-import com.sprint.mission.discodeit.web.controller.channel.dto.res.ChannelFindResponseDTO;
+import com.sprint.mission.discodeit.web.controller.dto.req.ChannelCreateRequestDTO;
+import com.sprint.mission.discodeit.web.controller.dto.req.ChannelUpdateRequestDTO;
+import com.sprint.mission.discodeit.web.controller.dto.req.PrivateChannelCreateRequestDTO;
+import com.sprint.mission.discodeit.web.controller.dto.res.ChannelFindResponseDTO;
 import java.time.Instant;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -24,39 +23,33 @@ import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class ChannelAppServiceImpl implements ChannelAppService{
+public class ChannelServiceApp {
     private final ChannelService channelService;
     private final ReadStatusService readStatusService;
     private final MessageService messageService;
-    private final UserService userService;
 
     //퍼블릭 채널 저장
-    @Override
     public Channel makePublicChannel(ChannelCreateRequestDTO channelCreateRequestDTO) {
-        Channel channel = channelCreateRequestDTO.toEntity();
+        Channel channel = Channel.init(channelCreateRequestDTO.getChannelName(), channelCreateRequestDTO.getChannelType());
 
         return channelService.makeChannel(channel);
     }
 
     //프라이빗 채널 저장
-    @Override
     public Channel makePrivateChannel(
         PrivateChannelCreateRequestDTO privateChannelCreateRequestDTO) {
-        Channel channel = privateChannelCreateRequestDTO.toEntity();
+        Channel channel = Channel.init(privateChannelCreateRequestDTO.getChannelName(), privateChannelCreateRequestDTO.getChannelType());
 
         /*
             1. 채널저장
             2. 유저아이디 리스트로 들어온 것 + 채널 아이디 정보로 리드스테이터스 개체 일일이 생성
             3. 생성된 모든 개체 리드스테이터스 저장
          */
-        channelService.makeChannel(channel);
+        Channel makedChannel = channelService.makeChannel(channel);
 
         List<UUID> userIdList = privateChannelCreateRequestDTO.getUserList();
         List<ReadStatus> readStatuses = userIdList.stream()
-            .map(userId -> ReadStatus.builder()
-                .userId(userId)
-                .channelId(channel.getId())
-                .build()
+            .map(userId -> ReadStatus.init(userId, makedChannel.getId())
             )
             .toList();
 
@@ -64,10 +57,10 @@ public class ChannelAppServiceImpl implements ChannelAppService{
             readStatusService.createReadStatus(readStatus);
         }
 
-        return channel;
+        return makedChannel;
     }
 
-    @Override
+    //특정 채널을 조회하고 싶을 때
     public ChannelFindResponseDTO findChannel(UUID channelId) {
         /*
             1. 채널 찾기
@@ -80,14 +73,15 @@ public class ChannelAppServiceImpl implements ChannelAppService{
         if(channel.getChannelType().equals(ChannelType.PRIVATE_CHANNEL)){
             List<ReadStatus> readStatuses = readStatusService.findAllReadStatusByChannelId(channelId);
             userIdlist = readStatuses.stream()
-                .map(readStatus -> readStatus.getUserId())
+                .map(ReadStatus::getUserId)
                 .toList();
         }
 
-        Message message = messageService.findLastMessageByChannelId(channel.getId());
+
         Instant createdAt = null;
-        if(Objects.nonNull(message)) {
-            createdAt = message.getCreatedAt();
+        Optional<Message> message = messageService.findLastMessageByChannelId(channel.getId());
+        if(message.isPresent()){
+            createdAt = message.get().getCreatedAt();
         }
 
         return ChannelFindResponseDTO.builder()
@@ -104,11 +98,7 @@ public class ChannelAppServiceImpl implements ChannelAppService{
         [ ] PUBLIC 채널 목록은 전체 조회합니다.
         [ ] PRIVATE 채널은 조회한 User가 참여한 채널만 조회합니다.
      */
-    @Override
     public List<Channel> findAllByUserId(UUID userId) {
-        //검증
-        User user = userService.findById(userId);
-
         /*
             1. 채널서비스로 모든 채널을 가져온다.              //디비 한번 접근
             2. 리드스테이터스에서 유저가 속한 모든 채널아이디를 가져온다. (이건 프라이빗임)
@@ -119,14 +109,36 @@ public class ChannelAppServiceImpl implements ChannelAppService{
         List<Channel> channelList = channelService.findAllChannel();
         List<ReadStatus> readStatusList = readStatusService.findReadStatusByUserId(userId);
         Set<UUID> set = readStatusList.stream()
-            .map(readStatus -> readStatus.getChannelId())
+            .map(ReadStatus::getChannelId)
             .collect(Collectors.toSet());
 
         List<Channel> channels = channelList.stream()
             .filter(channel -> channel.getChannelType().equals(ChannelType.PUBLIC_CHANNEL)
-            || set.contains(channel.getId()))
+                || set.contains(channel.getId()))
             .toList();
 
         return channels;
+    }
+
+
+    /*
+        update
+        [ ] DTO를 활용해 파라미터를 그룹화합니다.
+        수정 대상 객체의 id 파라미터, 수정할 값 파라미터
+        [ ] PRIVATE 채널은 수정할 수 없습니다.
+        delete
+        [ ] 관련된 도메인도 같이 삭제합니다.
+        Message, ReadStatus
+     */
+    public Channel updateChannelName(ChannelUpdateRequestDTO channelUpdateRequestDTO){
+        return channelService.updateChannelName(channelUpdateRequestDTO.getChannelId(),
+            channelUpdateRequestDTO.getChannelName());
+    }
+
+    //dto 없이 단일 인자만 받음
+    public void deleteChannel(UUID channelId){
+        channelService.deleteChannel(channelId);
+        messageService.deleteMessageByChannelId(channelId);
+        readStatusService.deleteReadStatusByChannelId(channelId);
     }
 }
