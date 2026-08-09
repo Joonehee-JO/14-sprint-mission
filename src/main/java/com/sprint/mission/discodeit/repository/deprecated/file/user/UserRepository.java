4c6f6b9bc968ba8bc6e8c1a835fbd2ed0b373477
joonhee <parkes1801@naver.com>
2026-08-09 20:17:54 +0900

    refactor: 안쓰는 코드 삭제

--- src/main/java/com/sprint/mission/discodeit/repository/deprecated/file/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.repository.deprecated.file.user;
-
-import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.repository.deprecated.file.FileCrudRepository;
-import java.util.UUID;
-
-public interface UserRepository extends FileCrudRepository<User, UUID> {
-
-}
