a3aa7b1fe5af520c1b4e2056061b44efd02863c2
joonhee <parkes1801@naver.com>
2026-08-16 20:45:21 +0900

    refactor:

--- src/main/java/com/sprint/mission/discodeit/web/controller/user/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/web/controller/user/UserController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
         [ ] 사용자 정보를 수정할 수 있다. -> ok
         [ ] 사용자를 삭제할 수 있다. -> ok
         [ ] 모든 사용자를 조회할 수 있다. -> ok
-        [ ] 사용자의 온라인 상태를 업데이트할 수 있다. ->
+        [ ] 사용자의 온라인 상태를 업데이트할 수 있다. -> ok
  */
 @RequiredArgsConstructor
 @RestController
